4afb3ebdcbacae9a73cbc37d1a21154edac8285d
hwk <wankui.han@tengwow.com>
2023-07-19 21:31:29 +0800

    二级路由配置

---
+++ src/views/discover/c-views/artist/index.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react';
+import type { FC, ReactNode } from 'react';
+interface IProps {
+  children?: ReactNode;
+}
+const Artist: FC<IProps> = (props) => {
+  return <div>Artist</div>;
+};
+export default memo(Artist);
