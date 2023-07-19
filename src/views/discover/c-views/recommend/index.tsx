cf05428663f9397e87ce5071b1cb2782b044c49b
hwk <wankui.han@tengwow.com>
2023-07-19 23:52:12 +0800

    axios二次封装

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,9 +1,21 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import type { FC, ReactNode } from 'react';
+import hyRequest from '@/service';
 interface IProps {
   children?: ReactNode;
 }
 const Recommend: FC<IProps> = (props) => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(count);
+    hyRequest
+      .get({
+        url: '/banner',
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
   return <div>Recommend</div>;
 };
 export default memo(Recommend);
