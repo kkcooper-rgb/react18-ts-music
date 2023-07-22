d9731327a1cb648793c4bfe8638bdd7cb33c4660
hwk <wankui.han@tengwow.com>
2023-07-22 18:55:56 +0800

    tijiao

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React, { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from '@/router';
-import { useAppSelector, useAppDispatch } from '@/store';
-import { incrementByAmount } from '@/store/modules/counter';
+// import { useAppSelector, useAppDispatch } from '@/store';
+// import { incrementByAmount } from '@/store/modules/counter';
 import CommonHeader from './components/app-header/commonHeader';
 import AppFooter from './components/app-footer';
 // type GetStateFnType = typeof store.getState;
 // type IRootState = ReturnType<GetStateFnType>;
 function App() {
-  const { count, message } = useAppSelector((state) => ({
-    count: state.counter.count,
-    message: state.counter.message,
-  }));
-  const dispatch = useAppDispatch();
-  function handleCount() {
-    console.log('click');
-    dispatch(incrementByAmount(1));
-  }
+  // const { count, message } = useAppSelector((state) => ({
+  //   count: state.counter.count,
+  //   message: state.counter.message,
+  // }));
+  // const dispatch = useAppDispatch();
+  // function handleCount() {
+  //   console.log('click');
+  //   dispatch(incrementByAmount(1));
+  // }
   return (
     <div className='App'>
       <CommonHeader />
--- src/views/discover/index.tsx
+++ src/views/discover/index.tsx
@@ -1,11 +1,11 @@
 import React, { memo, Suspense } from 'react';
 import type { FC, ReactNode } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import NavBar from './c-nav';
 interface IProps {
   children?: ReactNode;
 }
-const Discover: FC<IProps> = (props) => {
+const Discover: FC<IProps> = () => {
   return (
     <div>
       <NavBar />
